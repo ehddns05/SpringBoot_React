27c74881d61f2dab8e2283746b0bb2d90c726076
dongdong <dongwun05@gmail.com>
2020-07-16 14:12:44 +0900

    test1

---
+++ React/my-app/src/todolist/work.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { decorate, observable, action } from 'mobx';
+import { observer } from 'mobx-react';
+
+class Work extends React.Component{
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+
+    }
+}
+
+export default Work;
